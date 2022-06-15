ef20909c5b494ddcf3bfedfeffccfa02a8dcee53
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-06-15 14:47:41 -0300

    Implement existsPositive

--- 2_functions.hs
+++ 2_functions.hs
@@ -1,5 +1,17 @@
--- Defina uma função para calcular a função ou-exclusivo (xor).
+--  Defina uma função para calcular a função ou-exclusivo (xor).
 
 xor' :: Bool -> Bool -> Bool
 xor' False value = value
 xor' True value = not value
+
+
+{--
+    Defina a função existsPositive que retorna verdadeiro se a lista de números inteiros fornecida como argumento possui
+    pelo menos um elemento maior que 0.
+--}
+
+existsPositive' :: [Int] -> Bool
+existsPositive' [] = False
+existsPositive' (x : xs)
+    | x > 0 = True
+    | otherwise = existsPositive' xs
