b18165cb7623f6a5a3c0bb01c80e213b92583aa0
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-06-16 08:47:46 -0300

    Create 4_lists_and_recursion.hs

---
+++ 4_lists_and_recursion.hs
@@ -0,0 +1,15 @@
+-- Defina a função minList que retorna o menor inteiro de uma lista de números fornecida como entrada.
+
+
+-- Implemente a função andList que produz a conjunção de uma lista de booleanos fornecida como entrada.
+
+
+-- Implemente a função orList que produz a disjunção de uma lista de booleanos fornecida como entrada.
+
+
+-- Implemente a função indexOf que, a partir de um inteiro x e uma lista de inteiros xs, retorna a posição de x na lista xs.
+-- Caso x não pertença a lista, o valor -1 deve ser retornado.
+
+
+-- Implemente a função removeAll que, a partir de um inteiro x e uma lista de inteiros xs, remove todas as ocorrências de x da lista xs.
+
