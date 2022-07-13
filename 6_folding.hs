b849bf4fba4c69c059da10b7073c1b7f5a7580ec
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-07-13 10:56:44 -0300

    Implement all function recursively

--- 6_folding.hs
+++ 6_folding.hs
@@ -13,3 +13,20 @@
 
 takeWhile' :: (a -> Bool) -> [a] -> [a]
 takeWhile' p = foldr (\x acc -> if (p x) then (x : acc) else []) []
+
+
+{-
+    A função all determina se todos os elementos de uma lista satisfazem um predicado. Seu tipo é:
+
+        all :: (a -> Bool) -> [a] -> Bool
+
+    Implemente a função all:
+        1) usando recursão
+        2) usando foldr.
+-}
+
+all1 :: (a -> Bool) -> [a] -> Bool
+all1 p [] = False
+all1 p (x : xs)
+    | p x = True && (all1 p xs)
+    | otherwise = False
