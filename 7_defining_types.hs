3636a190d5e5d32157429066740c7da7c7506f81
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-07-20 14:53:04 -0300

    Number of nodes in a BST
    Implement a function to calculate the number of nodes in a Integer Binary Search Tree.

--- 7_defining_types.hs
+++ 7_defining_types.hs
@@ -15,5 +15,16 @@ surface (Rectangle (Point (x1, y1)) (Point (x2, y2))) = (abs $ x2 - x1) * (abs $
 
 -- Desenvolva funções para calcular o número de elementos e o número de folhas de uma árvore de tipo IntTree.
 
+data IntTree = ILeaf | INode Int IntTree IntTree
+
+
+bst_n_elements :: IntTree -> Int
+bst_n_elements ILeaf = 0
+bst_n_elements (INode x lst rst) = 1 + (bst_n_elements lst) + (bst_n_elements rst)
+
+-- For tests: (INode 8 (INode 3 (INode 1 ILeaf ILeaf) (INode 6 (INode 4 ILeaf ILeaf) (INode 7 ILeaf ILeaf))) (INode 10 ILeaf (INode 14 (INode 13 ILeaf ILeaf) ILeaf)))
+
+
+
 -- Desenvolva uma função que a partir de uma lista de clientes, retorne uma lista contendo todos os clientes que desejam
 -- receber mensagens de ofertas.
