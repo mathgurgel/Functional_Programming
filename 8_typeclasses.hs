3981500d02d25b527b3181b203990430b88ee4c5
mathgurgel <88843455+mathgurgel@users.noreply.github.com>
2022-07-23 10:14:11 -0300

    Implement Show instance for Person

--- 8_typeclasses.hs
+++ 8_typeclasses.hs
@@ -37,3 +37,5 @@ instance Eq Person where
 -- Implemente uma instância de Show para Person de forma que o resultado da conversão para string de um valor de tipo Person
 -- exiba apenas o campo name deste registro.
 
+instance Show Person where
+    show (Person name _) = name
